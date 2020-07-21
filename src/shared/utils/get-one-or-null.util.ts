1a1de1d35a6970cd02e5e3eebce7ab0de56a6550
Alan Jhonnes <alanjhonnes@gmail.com>
2020-07-21 17:41:49 -0300

    feat(get-one-or-null): add unsafe method without null return

--- src/shared/utils/get-one-or-null.util.ts
+++ src/shared/utils/get-one-or-null.util.ts
@@ -4,3 +4,14 @@ export function getOneOrNull<T>(array: T[]): T | null {
     }
     return null;
 }
+
+/**
+ * Unsafe getOne function, fails if array is empty. Use only with garanteed non-empty arrays
+ * @param array 
+ */
+export function getOneOrFail<T>(array: T[]): T {
+    if(array.length > 0) {
+        return array[0];
+    }
+    throw new Error('Unable to get first result from array');
+}
