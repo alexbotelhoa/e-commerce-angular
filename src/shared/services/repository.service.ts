96b5e1f98536bcfc3d7375519b33e6c83235f170
Alan Jhonnes <alanjhonnes@gmail.com>
2020-07-21 17:41:17 -0300

    fix(repository.service): wrong return types on db service

--- src/shared/services/repository.service.ts
+++ src/shared/services/repository.service.ts
@@ -1,61 +1,62 @@
 import { DatabaseService } from "./database.service";
 import Knex from "knex";
 import { StringKeyof } from "../types/string-keyof.type";
-import { getOneOrNull } from "../utils/get-one-or-null.util";
+import { getOneOrNull, getOneOrFail } from "../utils/get-one-or-null.util";
 
 export const getEntityById =
     <T, IdType extends string | number = string | number>(table: string) =>
         (idKey: StringKeyof<T>) =>
-            (db: DatabaseService<T, T>) =>
+            (db: DatabaseService<T, T[]>) =>
                 (id: IdType): Promise<T | null> => {
                     return db.table(table)
                         .select('*')
                         .where(idKey, id)
-                        .then(getOneOrNull) as Promise<T | null>
+                        .then(getOneOrNull);
                 }
 
 export const getEntitiesByIds =
     <T, IdType extends string | number = string | number>(table: string) =>
         (idKey: StringKeyof<T>) =>
-            (db: DatabaseService<T, T>) =>
-                (ids: IdType[]): Promise<T[]> => {
-                    return db.table(table)
+            (db: DatabaseService<T, T[]>) =>
+                (ids: IdType[]): Knex.QueryBuilder<T, T[]> => {
+                    return db
                         .select('*')
+                        .from<T, T[]>(table)
                         .whereIn(idKey, ids)
                 }
 
 export const insertEntity =
     <T>(table: string) =>
-        (db: DatabaseService) =>
-            (data: Partial<T> | Array<Partial<T>>): Promise<number[]> => db.table(table).insert(data);
+            (db: DatabaseService) =>
+                (data: Partial<T> | Array<Partial<T>>): Promise<number> => db.insert(data).into(table).then(getOneOrFail);
 
 export const updateEntity =
     <T>(table: string) =>
-        (db: DatabaseService<T, T>) =>
+        (db: DatabaseService<T, T[]>) =>
             (data: Partial<T>) =>
-                (where: Knex.QueryCallback<T, T>): Promise<any> => db.table(table).update(data).where(where);
+                (where: Knex.QueryCallback<T, T[]>): Promise<any> => db.table(table).update(data).where(where);
 
 export const deleteEntity =
     <T>(table: string) =>
-        (db: DatabaseService<T, T>) =>
-            (where: Knex.QueryCallback<T, T>): Promise<any> =>
+        (db: DatabaseService<T, T[]>) =>
+            (where: Knex.QueryCallback<T, T[]>): Promise<any> =>
                 db.table(table).del().where(where);
 
 export const deleteAllEntities =
     <T>(table: string) =>
-        (db: DatabaseService<T, T>): Promise<any> =>
+        (db: DatabaseService<T, T[]>): Promise<any> =>
             db.table(table).del();
 
 export const selectEntity =
     <T>(table: string) =>
-        (db: DatabaseService<T, T>) =>
+        (db: DatabaseService<T, T[]>) =>
             async (where: Knex.QueryCallback<T, T>): Promise<T[]> =>
-                db.table(table).select('*').where(where);
+                db.select('*').from(table).where(where);
 
-export const createRepository = <T, IdType extends string | number = string | number>(table: string, idKey: StringKeyof<T>) => {
+export const createRepository = <T, IdType extends string | number = string | number>(table: string, primaryColumn: StringKeyof<T>) => {
     return {
-        getById: getEntityById<T, IdType>(table)(idKey),
-        getManyByIds: getEntitiesByIds<T, IdType>(table)(idKey),
+        getById: getEntityById<T, IdType>(table)(primaryColumn),
+        getManyByIds: getEntitiesByIds<T, IdType>(table)(primaryColumn),
         select: selectEntity<T>(table),
         insert: insertEntity<T>(table),
         update: updateEntity<T>(table),
