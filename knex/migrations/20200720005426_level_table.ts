79ccbca0aae783883b2cdb3b70b0ddad8eb7771c
Alan Jhonnes <alanjhonnes@gmail.com>
2020-07-21 09:42:00 -0300

    fix(level-migration): add nonNullable to active and createdAt

--- knex/migrations/20200720005426_level_table.ts
+++ knex/migrations/20200720005426_level_table.ts
@@ -1,13 +1,14 @@
 import * as Knex from "knex";
+import { LEVEL_TABLE } from "../../src/entities/level.entity";
 
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.createTable('level', (table) => {
+    await knex.schema.createTable(LEVEL_TABLE, (table) => {
         table.increments('id');
         table.string('name', 100).notNullable();
         table.integer('order', 3);
-        table.boolean('active').defaultTo(true);
-        table.timestamp('createdAt').defaultTo(knex.fn.now());
+        table.boolean('active').notNullable().defaultTo(true);
+        table.timestamp('createdAt').notNullable().defaultTo(knex.fn.now());
         table.index('order');
         table.index('active');
     });
@@ -15,6 +16,6 @@ export async function up(knex: Knex): Promise<void> {
 
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.dropTableIfExists('level');
+    await knex.schema.dropTableIfExists(LEVEL_TABLE);
 }
 
