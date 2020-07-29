db09cdfaaaad88cc5997bbc371a1c42c2b56a31f
Alan Jhonnes <alanjhonnes@gmail.com>
2020-07-29 20:06:27 -0300

    feat: add authorization resolvers

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -8,6 +8,8 @@ import { activityUnionResolvers } from "./domain/activity/types/activity.union";
 import { embeddedActivityResolvers } from "./domain/activity/types/activity-types/embedded/embedded-activity.resolvers";
 import { htmlActivityResolvers } from "./domain/activity/types/activity-types/html/html-activity.resolvers";
 import { RoleId } from "./domain/authorization/enums/role-id.enum";
+import { roleResolvers } from "./domain/authorization/types/role.type.resolvers";
+import { permissionResolvers } from "./domain/authorization/types/permission.type.resolvers";
 
 export const resolvers: GQLResolvers = <GQLResolvers>{
     Query: queryResolvers,
@@ -18,6 +20,8 @@ export const resolvers: GQLResolvers = <GQLResolvers>{
     RoleId: RoleId,
     EmbeddedActivity: embeddedActivityResolvers,
     HtmlActivity: htmlActivityResolvers,
+    Role: roleResolvers,
+    Permission: permissionResolvers,
 };
 
 
