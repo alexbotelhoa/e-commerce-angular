8cfc7dad5b88ecf5cfdaa386aae4ddbd8f92b4b7
Alan Jhonnes <alanjhonnes@gmail.com>
2020-08-07 10:12:46 -0300

    feat(level-codes-query): return result set ordered by id

--- src/domain/activity/queries/level-codes/level-codes.query.ts
+++ src/domain/activity/queries/level-codes/level-codes.query.ts
@@ -2,5 +2,5 @@ import { GQLQueryResolvers } from "../../../../resolvers-types";
 import { selectLevelCode } from "../../../../shared/repositories/level-code.repository";
 
 export const levelCodesQueryResolver: GQLQueryResolvers['levelCodes'] = (obj, params, context) => {
-    return selectLevelCode(context.database);
+    return selectLevelCode(context.database).orderBy('id', 'asc');
 }
