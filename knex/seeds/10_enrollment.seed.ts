62319dafa98976ac5db5470a4b6746b9b8ea4568
Alan Jhonnes <alanjhonnes@gmail.com>
2020-09-02 16:28:18 -0300

    feat(enrollment.seed): enroll student in more than one class

--- knex/seeds/10_enrollment.seed.ts
+++ knex/seeds/10_enrollment.seed.ts
@@ -1,7 +1,7 @@
 import * as Knex from "knex";
 import { deleteAllEnrollments, insertEnrollment } from "../../src/shared/repositories/enrollment.repository";
 import { studentUserSeed } from "./07_user.seed";
-import { basic1LevelCodeSeed, basic2LevelCodeSeed } from "./08_level_code.seed";
+import { basic1LevelCodeSeed, basic2LevelCodeSeed, intermediary1LevelCodeSeed, advanced2LevelCodeSeed } from "./08_level_code.seed";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
@@ -16,6 +16,16 @@ export async function seed(knex: Knex): Promise<void> {
         {
             id: 2,
             levelCodeId: basic2LevelCodeSeed.id,
+            userId: studentUserSeed.id,
+        },
+        {
+            id: 3,
+            levelCodeId: intermediary1LevelCodeSeed.id,
+            userId: studentUserSeed.id,
+        },
+        {
+            id: 4,
+            levelCodeId: advanced2LevelCodeSeed.id,
             userId: studentUserSeed.id,
         },
     ]);
