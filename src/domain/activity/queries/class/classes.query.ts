36b0993a0b9290c959d0eb1016da02392b4ecde0
Alan Jhonnes <alanjhonnes@gmail.com>
2020-09-23 16:11:58 -0300

    fix(classes.query): failing if passed empty arrays for filters

--- src/domain/activity/queries/class/classes.query.ts
+++ src/domain/activity/queries/class/classes.query.ts
@@ -4,19 +4,19 @@ import { selectClass } from "../../../../shared/repositories/class.repository";
 export const classesQueryResolver: GQLQueryResolvers['classes'] = async (obj, { data }, context) => {
     const query = selectClass(context.database);
     if (data) {
-        if (data.ids) {
+        if (data.ids && data.ids.length > 0) {
             query.whereIn('id', data.ids);
         }
-        if (data.carrerIds) {
+        if (data.carrerIds && data.carrerIds.length > 0) {
             query.whereIn('carrerId', data.carrerIds);
         }
-        if (data.institutionIds) {
+        if (data.institutionIds && data.institutionIds.length > 0) {
             query.whereIn('institutionId', data.institutionIds);
         }
-        if (data.periodIds) {
+        if (data.periodIds && data.periodIds.length > 0) {
             query.whereIn('periodId', data.periodIds);
         }
-        if (data.sessionIds) {
+        if (data.sessionIds && data.sessionIds.length > 0) {
             query.whereIn('sessionId', data.sessionIds);
         }
     }
