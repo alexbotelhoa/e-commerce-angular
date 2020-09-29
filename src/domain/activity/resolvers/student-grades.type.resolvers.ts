08e691e52a849118e7115b660794f8ed81c03913
Alan Jhonnes <alanjhonnes@gmail.com>
2020-09-29 08:02:12 -0300

    fix(student-grades): grades can be over 100 when old activities get completed

--- src/domain/activity/resolvers/student-grades.type.resolvers.ts
+++ src/domain/activity/resolvers/student-grades.type.resolvers.ts
@@ -1,6 +1,12 @@
 import { GQLStudentGradeResolvers } from "../../../resolvers-types";
 
 export const studentGradesResolvers: GQLStudentGradeResolvers = {
-    grade: obj => obj.grade,
+    grade: obj => {
+        // clamp the value here since it is possible the query counts old activities that may end up accounting for more than 100%
+        if (obj.grade > 100) {
+            return 100;
+        }
+        return obj.grade;
+    },
     typeId: obj => obj.typeId,
 }
