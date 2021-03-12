ea96ea7a82e86a2db4808569b5557a9b10748579
Diogo A. Miranda <diogo.miranda@outlook.com>
2021-03-12 12:53:27 -0300

    challenge seed

--- knex/seeds/data/challenges.data.ts
+++ knex/seeds/data/challenges.data.ts
@@ -5,70 +5,98 @@ export const challengesData: ChallengeEntity[] = [
         id: 1,
         text: 'Navegue pela plataforma e encontre os resources da primeira parte do seu curso.',
         startAt: new Date(2020, 7, 31),
+        endAt: new Date(2020, 7, 31),
+        active: true
     },
     {
         id: 2,
         text: 'Já planejou quando vai estudar na semana? Dica: planeje os horários de estudos',
         startAt: new Date(2020, 8, 7),
+        endAt: new Date(2020, 8, 7),
+        active: true
     },
     {
         id: 3,
         text: 'Qual sua atividade favorita do curso até agora? Compartilhe numa story @culturainglesasp',
         startAt: new Date(2020, 8, 14),
+        endAt: new Date(2020, 8, 14),
+        active: true
     },
     {
         id: 4,
         text: 'Tem participado dos eventos Culture4+U? Inscreva-se num evento pelo Portal do Aluno!',
         startAt: new Date(2020, 8, 21),
+        endAt: new Date(2020, 8, 21),
+        active: true
     },
     {
         id: 5,
         text: 'Já pediu ajuda ao e-tutor? Aproveite faça aquela perguntinha que ficou faltando!',
         startAt: new Date(2020, 8, 28),
+        endAt: new Date(2020, 8, 28),
+        active: true
     },
     {
         id: 6,
         text: 'Já terminou a parte 1 do seu curso? Confira em "Minhas Lições".',
         startAt: new Date(2020, 9, 5),
+        endAt: new Date(2020, 9, 5),
+        active: true
     },
     {
         id: 7,
         text: 'O que você aprendeu na plataforma hoje? Compartilhe com seus colegas!',
         startAt: new Date(2020, 9, 12),
+        endAt: new Date(2020, 9, 12),
+        active: true
     },
     {
         id: 8,
         text: 'Tem uma história divertida da sua infância? Conta pra gente numa story @culturainglesasp',
         startAt: new Date(2020, 9, 19),
+        endAt: new Date(2020, 9, 19),
+        active: true
     },
     {
         id: 9,
         text: 'Atividades em dia? Aproveite para treinar com o Speaking Boost!',
         startAt: new Date(2020, 9, 26),
+        endAt: new Date(2020, 9, 26),
+        active: true
     },
     {
         id: 10,
         text: 'Tem dúvidas enquanto estuda? Pergunte ao e-tutor no leão lá embaixo :)',
         startAt: new Date(2020, 10, 2),
+        endAt: new Date(2020, 10, 2),
+        active: true
     },
     {
         id: 11,
         text: 'Já terminou a parte 2 do seu curso? Confira em "Minhas Lições".',
         startAt: new Date(2020, 10, 9),
+        endAt: new Date(2020, 10, 9),
+        active: true
     },
     {
         id: 12,
         text: 'Qual o material da sua badge? Tente chegar ao próximo nível nessa semana!',
         startAt: new Date(2020, 10, 16),
+        endAt: new Date(2020, 10, 16),
+        active: true
     },
     {
         id: 13,
         text: 'Já marcou de bater papo com seus colegas fora do horário de aula?',
         startAt: new Date(2020, 10, 23),
+        endAt: new Date(2020, 10, 23),
+        active: true
     },
     {
         id: 14,
         text: 'Ficou alguma dúvida? Volte nos resources que já fez clicando em Minhas Lições.',
         startAt: new Date(2020, 10, 30),
+        endAt: new Date(2020, 10, 30),
+        active: true
     },
 ]
