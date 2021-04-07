d439ffe71c68712978a608f29ffbfa9b17c35ad0
Diogo A. Miranda <diogo.miranda@outlook.com>
2021-04-06 22:47:50 -0300

    fix(active-challenge):adjust query

--- src/domain/activity/queries/active-challenge/active-challenge.query.ts
+++ src/domain/activity/queries/active-challenge/active-challenge.query.ts
@@ -1,13 +1,23 @@
 import { GQLQueryResolvers } from "../../../../resolvers-types";
 import { selectChallenge } from "../../../../shared/repositories/challenge.repository";
 
-export const activeChallengeQueryResolver: GQLQueryResolvers['activeChallenge'] = async (obj, params, context) => {
-    const challenge = await selectChallenge(context.database)
-        .andWhere('startAt', '<=', new Date())
-        .orderBy('startAt', 'desc')
-        .first();
-    if (!challenge) {
-        throw new Error('Unable to find an active challenge');
+export const activeChallengeQueryResolver: GQLQueryResolvers['activeChallenge'] =
+    async (obj, params, context) => {
+        let actualDate = new Date().toISOString();
+        actualDate = actualDate.split('T')[0];
+
+        console.log('actualDate ', actualDate);
+
+        const challenge = await selectChallenge(context.database)
+            .where('startAt', '<=', actualDate)
+            .where('endAt', '>', actualDate)
+            .where('active', '=', true)
+            .orderBy('startAt', 'desc')
+            .first();
+
+        if (!challenge) {
+            throw new Error('Unable to find an active challenge');
+        }
+
+        return challenge;
     }
-    return challenge;
-}
