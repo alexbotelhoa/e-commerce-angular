a3879b7a4cc5c50a2090773b1a4234364a607583
jorge.junior <jorge.junior@mjv.com.br>
2021-04-21 15:12:41 -0300

    force migrations

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import fastifyJwt from 'fastify-jwt';
 import fastifyCors from 'fastify-cors';
 
 
+
 import { makeExecutableSchema, loadTypedefs, GraphQLFileLoader, mergeTypeDefs } from 'graphql-tools';
 import { resolvers } from './resolvers';
 import { environmentFactory } from './shared/services/environment.service';
