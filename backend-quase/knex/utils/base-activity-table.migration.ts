3e55b0b011e6378b081250a9e88469bfafa3aeb7
alex.almeida-at-944327110682 <alex.almeida@mjv.com.br>
2021-07-31 18:40:55 -0300

    Remove files old

--- backend-quase/knex/utils/base-activity-table.migration.ts
+++
@@ -1,6 +0,0 @@
-import Knex from "knex";
-
-export function baseActivityTableMigration(table: Knex.CreateTableBuilder): void {
-    table.integer('activityId').primary().unsigned();
-    table.foreign('activityId').references('activity.id').onDelete('CASCADE');
-}
--- backend-quase/knex/utils/set-utf8-table.migration.ts
+++
@@ -1,6 +0,0 @@
-import Knex from "knex";
-
-export function setUTF8Table(table: Knex.CreateTableBuilder): void {
-    table.charset('utf8mb4');
-    table.collate('utf8mb4_general_ci');
-}
