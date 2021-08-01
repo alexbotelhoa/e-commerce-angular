06fd9e803e193e2ce4f58f3493772518729f7ed9
alexbotelhoa <alexbotelho1@hotmail.com>
2021-08-01 17:21:59 -0300

    Add library bootstrap icons

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -3,8 +3,19 @@ import { NgModule } from '@angular/core';
 import { GraphQLModule } from './graphql/graphql.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ButtonAddComponent } from './components/button/button-add/button-add.component';
+import { ButtonEditComponent } from './components/button/button-edit/button-edit.component';
+import { ButtonCancelComponent } from './components/button/button-cancel/button-cancel.component';
+import { ButtonDeleteComponent } from './components/button/button-delete/button-delete.component';
 
-const sharedComponents = [HeaderComponent, FooterComponent];
+const sharedComponents = [
+  HeaderComponent,
+  FooterComponent,
+  ButtonAddComponent,
+  ButtonEditComponent,
+  ButtonCancelComponent,
+  ButtonDeleteComponent,
+];
 const sharedGraphql = [GraphQLModule]
 const sharedServices = [[]]
 
