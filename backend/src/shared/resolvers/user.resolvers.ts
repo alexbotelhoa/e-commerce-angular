02b90b6c356202552f788de283bf0e5ba908133a
alexbotelhoa <alexbotelho1@hotmail.com>
2021-08-13 20:45:42 -0300

    Addition new fields CPF and Telephone

--- backend/src/shared/resolvers/user.resolvers.ts
+++ backend/src/shared/resolvers/user.resolvers.ts
@@ -5,6 +5,8 @@ const userEntityResolvers: Pick<GQLUserResolvers, keyof UserEntity> = {
     id: obj => obj.id.toString(),
     name: obj => obj.name,
     email: obj => obj.email,
+    cpf: obj => obj.cpf,
+    telephone: obj => obj.telephone,
     createdAt: obj => obj.createdAt,
     updatedAt: obj => obj.updatedAt,
 }
