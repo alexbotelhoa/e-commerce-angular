97bbd5fb083496cda106a19e9dbe72170078a898
jorge.junior <jorge.nogueira.mazzatech>
2021-08-27 12:49:19 -0300

    FIX ANNOTATION DATES

--- src/shared/resolvers/annotation.resolver.ts
+++ src/shared/resolvers/annotation.resolver.ts
@@ -4,8 +4,8 @@ import { AnnotationEntity } from "../../entities/annotation.entity";
 export const annotationEntityResolvers: Pick<GQLAnnotationResolvers, keyof AnnotationEntity> = {
     userId: (obj) => obj.userId,
     meetingId: (obj) => obj.meetingId,
-    createdDate: (obj) => obj.createdDate,
-    updatedDate: (obj) => obj.updatedDate,
+    createdDate: (obj) => new Date(obj.createdDate).toISOString(), //("pt-BR", {timeZone: "America/Sao_Paulo"}),
+    updatedDate: (obj) => new Date(obj.updatedDate).toISOString(), //("pt-BR", {timeZone: "America/Sao_Paulo"}),
     data: (obj) => obj.data,
     id: (obj) => obj.id,
 }
