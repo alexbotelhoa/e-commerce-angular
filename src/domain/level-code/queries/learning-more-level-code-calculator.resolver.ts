eed12bd4f66897bf15b333e98b0420fe2d54f70b
alex.almeida-at-944327110682 <alex.almeida@mjv.com.br>
2021-11-26 10:08:51 -0300

    Fix in the learningMoreOption request GQL

--- src/domain/level-code/queries/learning-more-level-code-calculator.resolver.ts
+++ src/domain/level-code/queries/learning-more-level-code-calculator.resolver.ts
@@ -1,36 +1,34 @@
-import { LevelCodeEntity } from "../../../entities/level-code.entity";
 import { GQLQueryResolvers } from "../../../resolvers-types";
+import { LevelCodeEntity } from "../../../entities/level-code.entity";
 import { selectLevelCode } from "../../../shared/repositories/level-code.repository";
 
-type ReduceLearningMore =  {paginab2c: null | "paginab2c", eyoung: null | "eyoung", spboost: null | "spboost", podcast: null | "podcast"};
-
-
 export const getLearningMoreLevelCodeCalculatorResolver: GQLQueryResolvers['learningMoreOption'] = async (obj, params, context) => {
-    
-    if ("levelId" in params.filters) {
+
+    if (params.filters.levelId) {
         const levelCodes = await selectLevelCode(context.readonlyDatabase).where("levelId", params.filters.levelId);
         return determinateLearningMorePriorityReduce(levelCodes);
     } else {
         const user = context.currentUser;
         const userId = user?.id;
         const [levelCodes]: LevelCodeEntity[][] = await context.readonlyDatabase.raw(`
-        select
-            lc.*
-        from
-            class,
-            enrollment_class ec,
-            enrollment e,
-            user u,
-            level_code lc
-        where
-            u.id = e.userId
-            and e.id = ec.enrollmentId
-            and ec.classId = class.id
-            and lc.id = class.levelCodeId 
-            and u.id = ${userId}`)
-            return determinateLearningMorePriorityReduce(levelCodes)
-      
+            select
+                lc.*
+            from
+                class,
+                enrollment_class ec,
+                enrollment e,
+                user u,
+                level_code lc
+            where
+                u.id = e.userId
+                and e.id = ec.enrollmentId
+                and ec.classId = class.id
+                and lc.id = class.levelCodeId 
+                and u.id = ${userId}
+        `)
+        return determinateLearningMorePriorityReduce(levelCodes)
     }
+
 }
 
 function determinateLearningMorePriorityReduce(levelCode: LevelCodeEntity[]): LevelCodeEntity {
@@ -42,4 +40,3 @@ function determinateLearningMorePriorityReduce(levelCode: LevelCodeEntity[]): Le
     }, {paginab2c: null, eyoung: null, spboost: null, podcast: null} as any);
     return result.spboost || result.podcast || result.eyoung || result.paginab2c || null;
 }
-
