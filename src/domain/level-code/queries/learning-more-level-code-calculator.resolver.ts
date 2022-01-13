3591203d01c1f4d1aa18982ef388e228da8688a8
alex.almeida-at-944327110682 <alex.almeida@mjv.com.br>
2022-01-13 14:29:42 -0300

    Add kanttum attribute for teachers

--- src/domain/level-code/queries/learning-more-level-code-calculator.resolver.ts
+++ src/domain/level-code/queries/learning-more-level-code-calculator.resolver.ts
@@ -3,38 +3,44 @@ import { LevelCodeEntity } from "../../../entities/level-code.entity";
 import { selectLevelCode } from "../../../shared/repositories/level-code.repository";
 
 export const getLearningMoreLevelCodeCalculatorResolver: GQLQueryResolvers['learningMoreOption'] = async (obj, params, context) => {
+    const isTeacher = context.currentUser?.roleIds.some(teacher => teacher === 2);
+
     if (params.filters.levelId) {
         const levelCodes = await selectLevelCode(context.readonlyDatabase).where("levelId", params.filters.levelId);
-        return determinateLearningMorePriorityReduce(levelCodes);
+        return determinateLearningMorePriorityReduce(levelCodes, isTeacher);
     } else {
         const user = context.currentUser;
         const userId = user?.id;
         const [levelCodes]: LevelCodeEntity[][] = await context.readonlyDatabase.raw(`
-            select
+            SELECT
                 lc.*
-            from
+            FROM
                 class,
                 enrollment_class ec,
                 enrollment e,
                 user u,
                 level_code lc
-            where
+            WHERE
                 u.id = e.userId
-                and e.id = ec.enrollmentId
-                and ec.classId = class.id
-                and lc.id = class.levelCodeId 
-                and u.id = ${userId}
-        `)
-        return determinateLearningMorePriorityReduce(levelCodes)
+                AND e.id = ec.enrollmentId
+                AND ec.classId = class.id
+                AND lc.id = class.levelCodeId 
+                AND u.id = ${userId}
+        `);
+        return determinateLearningMorePriorityReduce(levelCodes, isTeacher);
     }
 }
 
-function determinateLearningMorePriorityReduce(levelCode: LevelCodeEntity[]): LevelCodeEntity {
+function determinateLearningMorePriorityReduce(levelCode: LevelCodeEntity[], isTeacher?: boolean): LevelCodeEntity {
     const result = levelCode.reduce((acc, item) => {
         if (item.learningMore) {
-            acc[item.learningMore]  = item;
+            acc[item.learningMore] = item;
         }
         return acc;
-    }, {kanttum: null, paginab2c: null, eyoung: null, spboost: null, podcast: null} as any);
-    return result.kanttum || result.spboost || result.podcast || result.eyoung || result.paginab2c || null;
+    }, { kanttum: null, spboost: null, podcast: null, eyoung: null, paginab2c: null } as any);
+
+    if (isTeacher) {
+        return result.kanttum || result.spboost || result.podcast || result.eyoung || result.paginab2c || null;
+    }
+    return result.spboost || result.podcast || result.eyoung || result.paginab2c || null;
 }
