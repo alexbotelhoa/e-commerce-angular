abf061936949387ea53b5a6efb8fe1d69bdf7420
alex.almeida-at-944327110682 <alex.almeida@mjv.com.br>
2022-01-24 09:23:25 -0300

    Added orderby in the graphql query logs

--- src/resolvers-types.ts
+++ src/resolvers-types.ts
@@ -35,6 +35,8 @@ import { StudentGrade } from './domain/activity/types/student-grade.type';
 import { GraphQLContext } from './shared/types/context.type';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 export type EnumResolverSignature<T, AllowedValues = any> = { [key in keyof T]?: AllowedValues };
 export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
@@ -967,6 +969,7 @@ export type GQLLogsQueryInput = {
   readonly search: Maybe<Scalars['String']>;
   readonly perPage: Maybe<Scalars['ID']>;
   readonly page: Maybe<Scalars['ID']>;
+  readonly orderBy: Maybe<Scalars['String']>;
 };
 
 export type GQLOccResult = {
