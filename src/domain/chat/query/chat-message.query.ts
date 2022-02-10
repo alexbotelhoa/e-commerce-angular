a2dda4845cd3a765265fc0f9dff9f4903a8be4bf
marcelo.silva <marcelo.silva@mjv.com.br>
2022-02-10 09:03:04 -0300

    Changed the order of the chat messages to createdAt asc

--- src/domain/chat/query/chat-message.query.ts
+++ src/domain/chat/query/chat-message.query.ts
@@ -6,6 +6,7 @@ export const chatMessageQueryResolver: GQLQueryResolvers['chatMessage'] = async 
 
   if (userId) {
     query.where("userId", "like", userId);
+    query.orderBy("createdAt", "asc");
   }
 
   return await query;
